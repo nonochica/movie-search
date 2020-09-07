f7a33c15f82be33e477f4cb8b82e5b30fc45d602
Antonio Chica <antonio.chicajimenez@gmail.com>
2020-09-07 12:37:50 +0200

    eliminando /build del git-ignore

---
+++ build/precache-manifest.722668715ba78763b2ae694d27fb6e13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cb14b43b31dec20d063bdd5e43f0d0",
+    "url": "/movie-search.git/index.html"
+  },
+  {
+    "revision": "adc07251711a9e22ddee",
+    "url": "/movie-search.git/static/css/main.ddad7b9e.chunk.css"
+  },
+  {
+    "revision": "71752caffa7d010587a5",
+    "url": "/movie-search.git/static/js/2.7ac77330.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search.git/static/js/2.7ac77330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc07251711a9e22ddee",
+    "url": "/movie-search.git/static/js/main.9fdc95c4.chunk.js"
+  },
+  {
+    "revision": "329a67e814ace569483e",
+    "url": "/movie-search.git/static/js/runtime-main.aefe9b6d.js"
+  },
+  {
+    "revision": "e99225ec1f861339d12f1dc0a5688593",
+    "url": "/movie-search.git/static/media/Cartel cinema.e99225ec.png"
+  },
+  {
+    "revision": "6f8e64c029a899990bbc43d6846d7f0e",
+    "url": "/movie-search.git/static/media/Tickets.6f8e64c0.png"
+  }
+]);

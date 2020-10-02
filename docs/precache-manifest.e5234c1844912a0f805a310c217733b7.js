6fe76ce51561903a723923a17859799211fd6e78
Antonio Chica <antonio.chicajimenez@gmail.com>
2020-10-02 12:55:00 +0200

    docs agregado

---
+++ docs/precache-manifest.e5234c1844912a0f805a310c217733b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e5bfe835ddf7255e320b6ac863b97d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b611c2d25dff42524a4",
+    "url": "/static/css/main.ddad7b9e.chunk.css"
+  },
+  {
+    "revision": "71752caffa7d010587a5",
+    "url": "/static/js/2.7ac77330.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ac77330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b611c2d25dff42524a4",
+    "url": "/static/js/main.f0af5492.chunk.js"
+  },
+  {
+    "revision": "1dad2f5df98dc5f30250",
+    "url": "/static/js/runtime-main.0f48a05b.js"
+  },
+  {
+    "revision": "e99225ec1f861339d12f1dc0a5688593",
+    "url": "/static/media/Cartel cinema.e99225ec.png"
+  },
+  {
+    "revision": "6f8e64c029a899990bbc43d6846d7f0e",
+    "url": "/static/media/Tickets.6f8e64c0.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-search.git/precache-manifest.722668715ba78763b2ae694d27fb6e13.js"
+  "/precache-manifest.e5234c1844912a0f805a310c217733b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-search.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
